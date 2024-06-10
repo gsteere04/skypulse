45fbbfb346ed76b9822f4f282d4ba574a7473c7a
Grant <gsteere4717@xmail.dixietech.edu>
2024-06-10 09:21:14 -0600

    fix me please

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const apiURL = 'https://api.weather.gov/';
+fetch(apiURL)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error', error);
+    });
+
